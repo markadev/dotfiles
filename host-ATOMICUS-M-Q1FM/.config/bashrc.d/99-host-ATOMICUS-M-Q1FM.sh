d1c9c9b047b8b8ad197544c0e98fdb5517bad70d
Mark Aikens <17986295+markadev@users.noreply.github.com>
2018-01-24 08:38:04 -0500

    ATOMICUS-M-Q1FM tweaks

--- host-ATOMICUS-M-Q1FM/.config/bashrc.d/99-host-ATOMICUS-M-Q1FM.sh
+++ host-ATOMICUS-M-Q1FM/.config/bashrc.d/99-host-ATOMICUS-M-Q1FM.sh
@@ -3,11 +3,12 @@ export CLICOLOR=1
 export PGHOST="127.0.0.1"
 export PGUSER="postgres"
 export PATH="${PATH}:/usr/local/opt/postgresql@9.4/bin"
+export TF_PLUGIN_CACHE_DIR="${HOME}/.cache/terraform-plugin-cache"
 
 npvenv()
 {
     . ${HOME}/src/platform-core/npvenv/bin/activate
 }
 
-alias bgrep="grep --exclude='*.pyc' --exclude='tags' --exclude-dir='.git' --exclude-dir='.build' --exclude-dir='.venv' --exclude-dir='npvenv' --exclude-dir='.terraform' --exclude-dir=.tox"
+alias bgrep="grep --exclude='*.pyc' --exclude='tags' --exclude='*.tfstate*' --exclude-dir='.git' --exclude-dir='.build' --exclude-dir='.venv' --exclude-dir='npvenv' --exclude-dir='.terraform' --exclude-dir=.tox"
 alias ecr_login="aws --region=us-east-1 ecr get-login |sh"
