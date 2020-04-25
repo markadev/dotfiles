07ce8c7b28bb901daeba5a0d181afaa4d500b5f4
Mark Aikens <17986295+markadev@users.noreply.github.com>
2020-04-25 10:30:43 -0400

    Fix cactus-vm configuration after Ubuntu upgrade

--- virtualenvwrapper/.config/bashrc.d/50-virtualenvwrapper.sh
+++ virtualenvwrapper/.config/bashrc.d/50-virtualenvwrapper.sh
@@ -1,1 +1,7 @@
-. virtualenvwrapper.sh
+if [ -f "${HOME}/.local/bin/virtualenvwrapper.sh" ]; then
+	. "${HOME}/.local/bin/virtualenvwrapper.sh"
+elif [ -f "/usr/share/virtualenvwrapper/virtualenvwrapper.sh" ]; then
+	. /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+else
+	. virtualenvwrapper.sh
+fi
